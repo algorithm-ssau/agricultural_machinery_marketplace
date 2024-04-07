9d47e95ad3c3e3a74d9ea38aa09295cd8121ef5a
ssablinaa <160714722+ssablinaa@users.noreply.github.com>
2024-04-07 23:24:04 +0400

    header

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,4 @@
 <script setup></script>
 <template>
-  <div class="bg-gray-800 w-4/5 m-auto">
-    <h1>Hello</h1>
-  </div>
+  <div class="bg-gray-800 w-full h-1/4 m-auto"></div>
 </template>
---
+++ src/component/Header.vue
@@ -0,0 +1,18 @@
+<template>
+  <header class="flex justify-between px-24">
+    <div>
+      <img src="/src/public/logo.png" alt="Logo" class="w-28" />
+    </div>
+    <ul class="flex items-center cursor-pointer">
+      <li class="flex items-center">
+        <img src="/src/public/like.svg" alt="Cart" class="w-24 h-24" />
+      </li>
+      <li class="flex items-center cursor-pointer">
+        <img src="/src/public/user.svg" alt="Cart" class="w-24 h-24" />
+      </li>
+      <li class="flex items-center cursor-pointer">
+        <img src="/src/public/basket.svg" alt="Cart" class="w-24 h-24" />
+      </li>
+    </ul>
+  </header>
+</template>
