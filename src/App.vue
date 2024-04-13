79c9dfe6a53697557e9fedf999be646dd5e1c443
ssablinaa <160714722+ssablinaa@users.noreply.github.com>
2024-04-13 16:23:16 +0400

    catpictures

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <script setup>
 import Header from './component/Header.vue'
 import UnderHeader from './component/UnderHeader.vue'
+import CatImage from './component/CatImage.vue'
 </script>
 <template>
   <div class="bg-gray-800 w-full h-1/4 m-auto">
     <Header />
     <UnderHeader />
+    <CatImage />
   </div>
 </template>
---
+++ src/component/CatImage.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="w-full flex justify-center mt-5 mb-5">
+    <div class="mr-5 mb-5">
+      <img src="/src/public/catcar.svg" alt="Cart" class="w-max h-96" />
+    </div>
+    <div class="w -full mr-5">
+      <img src="/src/public/otvert.svg" alt="Cart" class="h-96" />
+    </div>
+    <div>
+      <div class="w-full">
+        <img src="/src/public/bank.svg" alt="Cart" class="h-44 mb-7" />
+      </div>
+      <div>
+        <img src="/src/public/ach.svg" alt="Cart" class="h-44" />
+      </div>
+    </div>
+  </div>
+</template>
