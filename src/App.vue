8c6e3efd066882c3efe4586c57f622a20174ffbd
ssablinaa <160714722+ssablinaa@users.noreply.github.com>
2024-04-26 23:01:28 +0400

    cardone

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,15 @@
 import Header from './component/Header.vue'
 import UnderHeader from './component/UnderHeader.vue'
 import CatImage from './component/CatImage.vue'
+import Card from './component/Card.vue'
 </script>
 <template>
   <div class="bg-gray-800 w-full h-1/4 m-auto">
     <Header />
     <UnderHeader />
     <CatImage />
+    <div class="bg-slate-200">
+      <Card />
+    </div>
   </div>
 </template>
---
+++ src/component/Card.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="relative m-10 bg-white h-80 w-80 border-slate-100 cursor-pointer">
+    <div class="flex items-center">
+      <img src="/src/public/like1.svg" alt="Like" class="ml-auto mr-4 mt-4" />
+    </div>
+    <div class="flex justify-center items-center">
+      <img src="/src/public/cart1.svg " alt="Cart1" class="h-48 w-52" />
+    </div>
+    <hr class="border-y-orange-500" />
+    <p class="ml-2">Обработка почвы 1</p>
+    <div class="flex justify-between">
+      <div class="flex flex-col ml-2">
+        <span class="font-sans">Цена:</span>
+        <span class="">3400р</span>
+      </div>
+      <div
+        class="rounded-md h-16 w-16 flex justify-center items-center border-solid border-2 border-orange-500"
+      >
+        <img src="/src/public/shopping.svg" alt="Plus" class="" />
+      </div>
+    </div>
+  </div>
+</template>
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Questrial&family=Quicksand:wght@300..700&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap');
+</style>
