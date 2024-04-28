cbc022a6795c4d0eda57abd0348aed126ae2c4e8
anMandel <145050983+AnastasiaMandel@users.noreply.github.com>
2024-04-28 19:52:32 +0400

    Add files via upload
    okno registra

--- back/app.py
+++ back/app.py
@@ -2,12 +2,14 @@
 This script runs the application using a development server.
 It contains the definition of routes and views for the application.
 """
-
-from flask import Flask
+# -*- coding: utf-8 -*-
+from flask import Flask, render_template, redirect, url_for, request
 from flask_sqlalchemy import SQLAlchemy
+#from app import db
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:Waterandwater63@localhost/backend'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 # Make the WSGI interface available at the top level so wfastcgi can get it.
@@ -21,28 +23,38 @@ class Technic(db.Model):
     working_width = db.Column(db.Integer, unique = False, nullable = False)
     description = db.Column(db.String(120), unique = True, nullable = True)
 
+    def __repr__(self):
+        return f'Type technic {self.type_technic}>'
+
 class User(db.Model):
     idUser = db.Column(db.Integer, primary_key = True)
     login = db.Column(db.String(30), unique = True, nullable = False)
     password = db.Column(db.String(30), unique = True, nullable = False)
 
+    def __repr__(self):
+        return f'User {self.login}>'
+
 class Order(db.Model):
     idOrder = db.Column(db.Integer, primary_key = True)
 
     idTechnic = db.Column(db.Integer, db.ForeignKey('technic.idTechnic'), nullable = False)
-    technic = db.relationship('Technic', backref = db.backref('id', lazy = True))
 
     idUser = db.Column(db.Integer, db.ForeignKey('user.idUser'), nullable = False)
-    user = db.relationship('User', backref = db.backref('number', lazy = True))
+
+    def __repr__(self):
+        return f'Number order {self.idOrder}>'
 
 app.app_context().push()
 db.create_all()
 
-
 @app.route('/')
-def hello():
-    """Renders a sample page."""
-    return "Hello World!"
+def index():
+    return render_template("C:\Users\mande\source\repos\web_project\static\templates\WebPage1.html")
 
+@app.route('/adm_login', methods=['GET', 'POST'])
+def admin_login():
+    if request.method == 'POST':
+        return render_template('login_adm.html')
 if __name__ == '__main__':
     app.run(debug = True)
+
