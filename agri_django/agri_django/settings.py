3e5c58b3e096ebb40a49d7b555724c4eeccb1206
anMandel <mandel.nastya@yandex.ru>
2024-05-24 21:19:42 +0400

    product

--- agri_django/agri_django/settings.py
+++ agri_django/agri_django/settings.py
@@ -42,6 +42,8 @@ INSTALLED_APPS = [
     'rest_framework.authtoken',
     'corsheaders',
     'djoser',
+
+    'product'
 ]
 
 CORS_ALLOW_ORIGINS = [
---
+++ agri_django/product/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+class Category(model.Model):
+    name = models.CharField(max_length=255)
+    slug = models.SlugField()
+
+    class Meta:
+        ordering = ('name',)
+
+    def __str__(self):
+        return self.name
+    
+    def get_absolute_url(self):
+        return f'/{self.slug}/'
+    
+    
