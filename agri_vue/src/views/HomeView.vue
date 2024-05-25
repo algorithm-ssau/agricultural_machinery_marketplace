2462d99d43ab2ff272e2830f2d5cbba203377aa9
anMandel <mandel.nastya@yandex.ru>
2024-05-25 13:37:13 +0400

    new_vue

--- agri_vue/src/views/HomeView.vue
+++ agri_vue/src/views/HomeView.vue
@@ -4,13 +4,35 @@
       <div class="hero-body has-text-centered">
         <p class="title mb-6">
           Маркетплейс сельскохозяйственной техники
-      </p>
-      <p class ="subtitle">
+        </p>
+        <p class ="subtitle">
             Выгодная покупка и продажа товаров
-      </p>
+        </p>
+      </div>
+    </section>
+
+    <div class="columns is-multiline">
+      <div class="column is-12">
+        <h2 class="is-size-2 has-text-centered">Последние добавленные товары</h2>
+      </div>
+      
+      <div class="column is-3" 
+      v-for="product in latestProducts"
+      v-bind:key="product.id" 
+      >
+      <div class ="box">
+        <figure class="image mb-4">
+          <img v-bind:src="product.get_thumbnail">
+        </figure>
+
+        <h3 class ="is-size-4">{{ product.name }}</h3>
+        <p class="is-size-6 has-text-grey">${{ product.price }}</p>
+
+        Посмотреть подробнее
+      </div>
     </div>
-</section>
-    </div>
+  </div>
+  </div>
 </template>
 
 <script>
