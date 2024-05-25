3b8cf3604f648782b0a888c6befe2253ee0925c6
anMandel <mandel.nastya@yandex.ru>
2024-05-25 13:17:16 +0400

    slogan dobavila

--- agri_vue/src/views/HomeView.vue
+++ agri_vue/src/views/HomeView.vue
@@ -1,5 +1,16 @@
 <template>
-  <div class="home">Home</div>
+  <div class="home">
+    <section class="hero is-medium is-dark mb-6">
+      <div class="hero-body has-text-centered">
+        <p class="title mb-6">
+          Маркетплейс сельскохозяйственной техники
+      </p>
+      <p class ="subtitle">
+            Выгодная покупка и продажа товаров
+      </p>
+    </div>
+</section>
+    </div>
 </template>
 
 <script>
