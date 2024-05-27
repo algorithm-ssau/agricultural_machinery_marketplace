fbf24feeb233f5ac00dec1d23dcbf522c4ce0431
PeterLonghand <onlywayisdutch@yandex.ru>
2024-05-27 07:32:08 +0400

    +возможность добавл. в Корзину

--- agri_vue/src/store/index.js
+++ agri_vue/src/store/index.js
@@ -1,14 +1,29 @@
-import { createStore } from 'vuex'
+import { createStore } from "vuex";
 
 export default createStore({
-  state: {
-  },
-  getters: {
-  },
+  state: {},
+  getters: {},
   mutations: {
+    initializeStore(state) {
+      if (localStorage.getItem("cart")) {
+        state.cart = JSON.parse(localStorage.getItem("cart"));
+      } else {
+        localStorage.setItem("cart", JSON.stringify(state.cart));
+      }
+    },
+    addToCart(state, item) {
+      const exists = state.cart.filter((i) => i.product.id === item.product.id);
+
+      if (exists.length) {
+        exists[0].quantity =
+          parseInt(exists[0].quantity) + parseInt(item.quantity);
+      } else {
+        state.cart.items.push(item);
+      }
+
+      localStorage.setItem("cart", JSON.stringify(state.cart));
+    },
   },
-  actions: {
-  },
-  modules: {
-  }
-})
+  actions: {},
+  modules: {},
+});
